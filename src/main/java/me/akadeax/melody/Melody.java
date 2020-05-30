98e369ea413a6a3e85569713675b816bd76f2987
Patrick <patrickqwertz@gmail.com>
2020-05-30 22:22:02 +0200

    Added JavaDoc

--- src/main/java/me/akadeax/melody/Melody.java
+++ src/main/java/me/akadeax/melody/Melody.java
@@ -5,10 +5,29 @@ import org.bukkit.entity.Player;
 import java.util.List;
 
 public interface Melody {
-    void test();
+    /**
+     * Plays a track at a players position
+     * @param track the track to play
+     * @param player the player that should be able to hear the track
+     */
     void playTrack(MelodyTrack track, Player player);
 
+    /**
+     * substitute for MelodyTrack constructor
+     * @param sounds the sounds the track should consist of
+     * @return the track that was created
+     */
     MelodyTrack createTrack(List<MelodyNote> sounds);
+
+    /**
+     * Substitute for MelodyNote constructor
+     * MelodyInstrument.Pause indicates a pause of length "octave"
+     */
     MelodyNote createNote(MelodyInstrument instrument, MelodyTone tone, int octave);
+
+    /**
+     * Returns a note that indicates a pause for 'time' seconds
+     */
+    MelodyNote createPause(int time);
 }
 
