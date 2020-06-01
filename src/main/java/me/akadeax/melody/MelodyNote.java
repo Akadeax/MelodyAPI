bd4a696487f4ffd0427babb1ad9386e2621e9da2
Patrick <patrickqwertz@gmail.com>
2020-06-01 17:23:44 +0200

    added constants to MelodyNote Changed java to Language level 8

--- src/main/java/me/akadeax/melody/MelodyNote.java
+++ src/main/java/me/akadeax/melody/MelodyNote.java
@@ -5,17 +5,28 @@ import org.bukkit.Note;
 public interface MelodyNote {
     MelodyInstrument getInstrument();
     MelodyTone getTone();
+    int getOctave();
 
     /**
      * Converts this Note into a bukkit note (so it can be played in-game)
      * @return the org.bukkit.Note version of this obj
      */
     Note getBukkitNote();
-    int getOctave();
+
+
+    /**
+     * Determines whether this note is a pause
+     */
     boolean isPause();
 
     /**
      * Determines whether this note is in between F#0 and F#2 (Minecraft's supported sound range)
      */
     boolean isPlayableInGame();
+
+
+    /**
+     * The amount of different tones in different octaves bukkit can play (F#0 - F#2) -> 25
+     */
+    static int bukkitToneAmount() { return 25; }
 }
