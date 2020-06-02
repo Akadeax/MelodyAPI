130caab829ecaa6add5c73a4e309c9d410147d11
Patrick <patrickqwertz@gmail.com>
2020-06-02 19:53:52 +0200

    Added Support for all Bukkit Instruments

--- src/main/java/me/akadeax/melody/MelodyInstrument.java
+++ src/main/java/me/akadeax/melody/MelodyInstrument.java
@@ -5,7 +5,21 @@ import org.bukkit.Instrument;
 public enum MelodyInstrument {
     PAUSE,
     PIANO,
-    BELL;
+    BASS_DRUM,
+    BASS_GUITAR,
+    BELL,
+    BIT,
+    CHIME,
+    COW_BELL,
+    DIDGERIDOO,
+    FLUTE,
+    GUITAR,
+    IRON_XYLOPHONE,
+    BANJO,
+    PLING,
+    SNARE_DRUM,
+    STICKS,
+    XYLOPHONE;
 
     public Instrument toBukkitInstrument() {
         return Instrument.valueOf(name());
